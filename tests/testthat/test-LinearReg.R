45bf67f78b551f084d2355c557d09a14a1a385b1
yw0817 <yw015@bucknell.edu>
2021-12-05 01:13:46 -0500

    update testthat

--- tests/testthat/test-LinearReg.R
+++ tests/testthat/test-LinearReg.R
@@ -1,8 +1,7 @@
 test_that("LinearReg works", {
     my.result = LinearReg(mpg ~ cyl + disp + hp + cyl*disp, data = mtcars)
     ref = lm(mpg ~ cyl + disp + hp + cyl*disp, data = mtcars)
-
-    for (i in 1:length(my.result$coefficients)) {
+    for (i in 1:length(ref$coefficients)) {
         expect_lt(abs(my.result$coefficients[i] - ref$coefficients[i]), 0.00001)
     }
     for (i in 1:length(ref$residuals)) {
@@ -13,4 +12,17 @@ test_that("LinearReg works", {
     expect_equal(my.result$df, ref$df)
     expect_equal(my.result$`Multiple R-squared`, summary(ref)$r.squared)
     expect_equal(my.result$`Adjusted R-squared`, summary(ref)$adj.r.squared)
+    # model without intercept
+    my.result2 = LinearReg(mpg ~ -1 + cyl + disp + hp + cyl*disp, data = mtcars)
+    ref2 = lm(mpg ~ -1 + cyl + disp + hp + cyl*disp, data = mtcars)
+    for (i in 1:length(ref2$coefficients)) {
+        expect_lt(abs(my.result2$coefficients[i] - ref2$coefficients[i]), 0.00001)
+    }
+    for (i in 1:length(ref2$residuals)) {
+        expect_lt(abs(my.result2$residuals[i] - ref2$residuals[i]), 0.00001)
+        expect_lt(abs(my.result2$fitted.values[i] - ref2$fitted.values[i]), 0.00001)
+    }
+    expect_equal(my.result2$rank, ref2$rank)
+    expect_equal(my.result2$df, ref2$df)
+    expect_equal(my.result2$`Multiple R-squared`, summary(ref2)$r.squared)
 })
--- tests/testthat/test-StepSelect.R
+++ tests/testthat/test-StepSelect.R
@@ -10,8 +10,10 @@ test_that("StepSelect works", {
     expect_equal(my$call$formula, ref$call$formula)
     # "forward selection"
     m = lm(mpg ~ 1, data = mtcars)
-    ref2 = stepAIC(m, direction="forward", k = log(n), scope=list(lower=m, upper=full_lm), trace = FALSE)
-    my2 = StepSelect(full_lm, direction = "forward", k = log(n), trace = FALSE)
+    ref2 = stepAIC(m, direction="forward", k = log(n), scope=list(lower=m, upper=full_lm), trace = TRUE)
+    my2 = StepSelect(full_lm, direction = "forward", k = log(n), trace = TRUE)
     expect_equal(my2$coefficients, ref2$coefficients)
     expect_equal(my2$call$formula, ref2$call$formula)
+
+    expect_warning(StepSelect(full_lm, direction = "forw", k = log(n), trace = TRUE), "Error in direction: should be 'backward' or 'forward' ")
 })
